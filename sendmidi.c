c234d23936c5f0509085aa45b8fca0efdfdfafa3
d <d@365922.local>
2021-03-26 13:32:56 -0500

    another one bites the dust

--- sendmidi.c
+++ sendmidi.c
@@ -11,7 +11,7 @@ void sendMidi(int number, int value) {
     char *program = sendMidiPath;
     strcat(command, program);
 
-    char *options = " dev IAC\ Driver cc ";
+    char *options = " dev IAC\\ Driver cc ";
     strcat(command, options);
     strcat(command, numberString);
 
